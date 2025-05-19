753722ff6815c511d9b114908f06f7389c2f6a9b
Nazulli <nazulliborn@gmail.com>
2025-05-18 18:38:37 -0700

    Create Made4LovingU.bat

---
+++ Made4LovingU.bat
@@ -0,0 +1,135 @@
+@echo off
+
+color 40
+timeout /t 2 >nul
+echo You are loved.
+
+echo.
+echo.
+
+timeout /t 3 >nul
+
+color 0C
+
+echo	I was made for loving you, baby 
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby 
+timeout /t 2 >nul
+echo	Can you get enough of me?
+
+
+echo.
+timeout /t 3 >nul
+echo.
+
+
+echo	Tonight I wanna give it all to you
+timeout /t 3 >nul
+echo	In the darkness there's so much I wanna do
+timeout /t 3 >nul
+echo	And tonight I wanna lay it at your feet
+timeout /t 2 >nul
+echo	'Cause, girl, I was made for you
+timeout /t 3 >nul
+echo	And, girl, you were made for me
+
+echo.
+timeout /t 4 >nul
+echo.
+
+
+echo	I was made for loving you, baby
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby
+timeout /t 2 >nul
+echo	Can you get enough of me?
+
+
+echo.
+timeout /t 4 >nul
+echo.
+
+
+echo	Tonight I wanna see it in your eyes
+timeout /t 2 >nul
+echo	Feel the magic, there's something that drives me wild
+timeout /t 2 >nul
+echo	And tonight we're going to make it all come true
+timeout /t 2 >nul
+echo	'Cause, girl, you were made for me
+timeout /t 2 >nul
+echo	And, girl, I was made for you
+
+
+echo.
+timeout /t 2 >nul
+echo.
+
+
+echo	I was made for loving you, baby
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby
+timeout /t 2 >nul
+echo	Can you get enough of me?
+
+
+echo.
+timeout /t 3 >nul
+echo.
+
+
+echo	I was made for loving you, baby
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby
+timeout /t 2 >nul
+echo	Can you get enough of me?
+
+
+echo.
+timeout /t 3 >nul
+echo.
+
+
+echo	I was made for loving you, baby
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby
+timeout /t 2 >nul
+echo	Can you get enough of me?
+
+
+echo.
+timeout /t 3 >nul
+echo.
+
+
+echo	I was made for loving you, baby
+timeout /t 2 >nul
+echo	You were made for loving me
+timeout /t 2 >nul
+echo	And I can't get enough of you, baby
+timeout /t 2 >nul
+echo	Can you get enough if me?
+timeout /t 3 >nul
+
+
+echo.
+echo.
+echo.
+
+
+timeout /t 6 >nul
+echo That's all. Goodbye.
+timeout /t 6
+
+
+exit
